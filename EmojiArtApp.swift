b811be7cd8fe04d934f7694d59d613a5a838dfea
redwan528 <58414163+redwan528@users.noreply.github.com>
2022-02-28 00:21:49 -0500

    Create EmojiArtApp.swift

---
+++ EmojiArtApp.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    let document = EmojiArtDocument()
+    
+    var body: some Scene {
+        WindowGroup {
+            EmojiArtDocumentView(document: document)
+        }
+    }
+}
